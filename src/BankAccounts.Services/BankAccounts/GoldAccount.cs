2de881a2d109ca1411699f8c4b9c5a3442e7fb7f
stergiazotali <gilmouR9>
2015-07-31 17:33:40 +0100

    .gitignore is now working

--- src/BankAccounts.Services/BankAccounts/GoldAccount.cs
+++ src/BankAccounts.Services/BankAccounts/GoldAccount.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankAccounts.Repository.Entities;
 
 namespace BankAccounts.Services.BankAccounts
 {
-    public class GoldAccount
+    public class GoldAccount : BankAccountLevelApprover
     {
-
+        public override BankAccountLevelApprover ProcessRequest(CustomerDto bankAccountApproval)
+        {
+            throw new NotImplementedException();
+            return null;
+        }
     }
 }
--- src/BankAccounts.Services/BankAccounts/SilverAccount.cs
+++ src/BankAccounts.Services/BankAccounts/SilverAccount.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankAccounts.Repository.Entities;
 
 namespace BankAccounts.Services.BankAccounts
 {
-    public class SilverAccount
+    public class SilverAccount: BankAccountLevelApprover
     {
+        public override BankAccountLevelApprover ProcessRequest(CustomerDto bankAccountApproval)
+        {
+            throw new NotImplementedException();
+            return null;
+        }
     }
 }
