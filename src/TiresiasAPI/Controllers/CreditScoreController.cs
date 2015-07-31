65e9a463ef24f4eb52010e47989d543d00d2cfdd
stergiazotali <gilmouR9>
2015-07-31 15:11:31 +0100

    Implement HadesAPI

---
+++ src/TiresiasAPI/Controllers/CreditScoreController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TiresiasAPI.Controllers
+{
+    public class CreditScoreController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
