50bb398b7c24217f90fa9ff34b7e8491a4edc6d1
icedage <stergiazotali@gmail.com>
2015-08-04 15:26:29 -0700

    Implementing microseervice

---
+++ src/BankAccounts.Services/Services/CustomerService.cs
@@ -0,0 +1,26 @@
+﻿using BankAccounts.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts.Services.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private ICreditReportService _creditReportService;
+
+        public CustomerService(ICreditReportService creditReportService)
+        {
+            _creditReportService = creditReportService;
+        }
+
+        public void CreateCustomer(CustomerDto customer)
+        {
+            var report = _creditReportService.GetCreditReport(customer);
+ 
+            throw new NotImplementedException();
+        }
+    }
+}
